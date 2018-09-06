4492666032bcf5058e4895bb56aaea38af5548c3
yepuri-r <42846974+yepuri-r@users.noreply.github.com>
2018-09-05 21:50:35 -0500

    Create Q17.py

---
+++ task3/Q17.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sea
+   
+
+df1= pd.read_csv("train.csv")
+df2= pd.read_csv("test.csv")
+combine = [df1,df2]
+titanic = pd.concat(combine)
+mode = titanic['Age'].mode()
+titanic['Age']=titanic['Age'].fillna('mode[0]')
+titanic['Age'].isnull().sum()
+
