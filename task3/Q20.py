fd694761d6345a0fbf24055a014aa306074f8bfc
yepuri-r <42846974+yepuri-r@users.noreply.github.com>
2018-09-05 23:03:01 -0500

    Create Q20.py

---
+++ task3/Q20.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sea
+   
+
+df1= pd.read_csv("train.csv")
+df2= pd.read_csv("test.csv")
+combine = [df1,df2]
+titanic = pd.concat(combine)
+for dataset in combine:
+    dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
+    dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+    dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
+    dataset.loc[ dataset['Fare'] > 31, 'Fare'] = 3
+    dataset['Fare'] = dataset['Fare'].astype(int)
+
+train_df = train_df.drop(['FareBand'], axis=1)
+combine = [train_df, test_df]
+    
+train_df.head(10)
