4da2e4d20b0ded81f0b12701b096e6718eecf74c
yepuri-r <42846974+yepuri-r@users.noreply.github.com>
2018-09-05 19:41:46 -0500

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas pd
+import matplotlib.pyplot as plt
+import seaborn as sea
+   
+
+df1= pd.read_csv("train.csv")
+df2= pd.read_csv("test.csv")
+combine = [df1,df2]
+titanic = pd.concat(combine)
+ titanic
