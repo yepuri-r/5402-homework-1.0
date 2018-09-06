5af027f2f9b5b564aa2798e36765ba58387a24cb
yepuri-r <42846974+yepuri-r@users.noreply.github.com>
2018-09-05 20:52:54 -0500

    Create Q14.py

---
+++ task3/Q14.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sea
+   
+
+df1= pd.read_csv("train.csv")
+df2= pd.read_csv("test.csv")
+combine = [df1,df2]
+titanic = pd.concat(combine)
+titanic['Ticket'].duplicated().sum()
+titanic['Ticket'].corr(titanic['Survived'])
